6f05d14e0af2faa2a4db7d287b13551d56251d3d
heiwang1997 <462149548@qq.com>
2017-05-25 22:49:46 +0800

    Modify some interfaces, need logic to be finished

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -1,14 +1,21 @@
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Pair;
 import tableElements.RSRow;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainSceneController implements Initializable {
@@ -44,12 +51,45 @@ public class MainSceneController implements Initializable {
 
     @FXML
     void inputClicked(ActionEvent event) {
+        String inputCode = "";
+        while (true) {
+            // Create the custom dialog.
+            Dialog<String> dialog = new Dialog<>();
+            dialog.setTitle("输入指令");
 
+            // Set the button types.
+            ButtonType loginButtonType = new ButtonType("确定", ButtonBar.ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+            TextArea tArea = new TextArea(inputCode);
+            tArea.setPromptText("请在这里输入代码");
+            BorderPane borderPane = new BorderPane(tArea);
+            BorderPane.setMargin(borderPane, new Insets(20, 20, 20, 20));
+
+            dialog.getDialogPane().setContent(borderPane);
+
+            // Convert the result to a username-password-pair when the login button is clicked.
+            dialog.setResultConverter(dialogButton -> {
+                if (dialogButton == loginButtonType) {
+                    return tArea.getText();
+                }
+                return null;
+            });
+
+            Optional<String> result = dialog.showAndWait();
+
+            if (result.isPresent()) {
+                inputCode = result.get();
+                showAlert(Alert.AlertType.ERROR, "错误", "语法错误", String.format("%s", result.get()));
+            } else {
+                return;
+            }
+        }
     }
 
     @FXML
     void startClicked(ActionEvent event) {
-
+        switchStatus(true);
     }
 
     @FXML
@@ -74,6 +114,8 @@ public class MainSceneController implements Initializable {
         clockLabel.setGraphic(new ImageView("/resources/time.png"));
 
         switchStatus(false);
+
+        pipeline = new Pipeline();
 
         refreshReservationStation();
     }
@@ -106,7 +148,22 @@ public class MainSceneController implements Initializable {
                 new RSRow("1"), new RSRow("2")
         );
         rsTableView.getItems().setAll(a);
+    }
 
+    private void showAlert(Alert.AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public void exitClicked(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void aboutClicked(ActionEvent actionEvent) {
+        showAlert(Alert.AlertType.INFORMATION, "信息", "关于本项目",
+                "本项目是计算机系统结构课程的第二次大作业，内容是Tomasulo算法模拟器。");
     }
 }
-
