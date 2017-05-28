c2e989239eb96066a49961959cc60db554fb0bf6
heiwang1997 <462149548@qq.com>
2017-05-28 16:42:32 +0800

    Finish nearly all UI design, start/restart bug needs to be fixed.

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -1,5 +1,5 @@
-import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,18 +7,19 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import javafx.util.Pair;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
 import tableElements.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.swing.*;
+import javax.swing.text.Style;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -28,7 +29,8 @@ public class MainSceneController implements Initializable {
 
     private final int SAMPLE_COUNT = 2;
     private final String[] RS_NAME = {"ADD", "MULT", "LOAD", "STORE"};
-    private final String[] OP_NAME = {"ADD", "SUB", "MUL", "DIV"};
+    private final String[] OP_NAME = {"ADD", "SUB", "MUL", "DIV", "LD", "ST"};
+    private final int[] CONTINUE_STEPS = {5, 10, 15, 20};
 
     public Label clockLabel;
     public Label clockTimeLabel;
@@ -38,8 +40,10 @@ public class MainSceneController implements Initializable {
     public TableView<CPURegRow> cpuRegTableView;
     public TableView<MemRow> memTableView;
     public Menu changeMenu;
+    public TableView<LsQueueRow> lsQueueTableView;
 
     private int currentTime;
+    private boolean timerRunning;
 
     @FXML
     private Button inputButton;
@@ -54,17 +58,12 @@ public class MainSceneController implements Initializable {
     private Button stepButton;
 
     @FXML
-    private Button continueButton;
+    private MenuButton continueButton;
 
     @FXML
     private Button tailButton;
 
     private Pipeline pipeline;
-
-    @FXML
-    void continueClicked(ActionEvent event) {
-
-    }
 
     @FXML
     void inputClicked(ActionEvent event) {
@@ -151,7 +150,15 @@ public class MainSceneController implements Initializable {
             currentTime = 0;
             pipeline.run();
             switchStatus(true);
+            // TODO: Okay to run this?
+            refreshAll();
         }
+    }
+
+    private void continueClicked(int step) {
+        pipeline.nextStep(step);
+        currentTime += step;
+        refreshAll();
     }
 
     @FXML
@@ -163,7 +170,24 @@ public class MainSceneController implements Initializable {
 
     @FXML
     void tailClicked(ActionEvent event) {
+        if (timerRunning) {
+            tailButton.setGraphic(new ImageView("/resources/tail.png"));
+            tailButton.setText("执行到底");
+            stepButton.setDisable(false);
+            continueButton.setDisable(false);
+            timerRunning = false;
+        } else {
+            tailButton.setGraphic(new ImageView("/resources/pause.png"));
+            tailButton.setText("暂停执行");
+            stepButton.setDisable(true);
+            continueButton.setDisable(true);
+            timerRunning = true;
+        }
+    }
 
+    private void backgroundSchedulePlay() {
+        if (!timerRunning) return;
+        stepClicked(null);
     }
 
     @Override
@@ -178,6 +202,7 @@ public class MainSceneController implements Initializable {
         clockLabel.setGraphic(new ImageView("/resources/time.png"));
         memTableView.setPlaceholder(new Label("内存中所有值均为0"));
         codeTableView.setPlaceholder(new Label("当前没有任何指令，点击左侧的输入指令或示例指令进行添加"));
+        lsQueueTableView.setPlaceholder(new Label("当前队列中没有值，所有Load-Store保留栈均为非Busy状态"));
 
         switchStatus(false);
 
@@ -211,6 +236,16 @@ public class MainSceneController implements Initializable {
             });
             sampleButton.getItems().add(sampleItem);
         }
+        for (int cs : CONTINUE_STEPS) {
+            MenuItem contItem = new MenuItem(String.format("执行%d步", cs));
+            contItem.setOnAction(event -> continueClicked(cs));
+            continueButton.getItems().add(contItem);
+        }
+        timerRunning = false;
+        Timeline backgroundTask = new Timeline(new KeyFrame(Duration.seconds(1),
+                event -> backgroundSchedulePlay()));
+        backgroundTask.setCycleCount(Timeline.INDEFINITE);
+        backgroundTask.play();
     }
 
     private void setClockTime(int time) {
@@ -236,6 +271,9 @@ public class MainSceneController implements Initializable {
             tailButton.setDisable(true);
             fpRegTableView.setEditable(true);
             changeMenu.setDisable(false);
+            tailButton.setGraphic(new ImageView("/resources/tail.png"));
+            tailButton.setText("执行到底");
+            timerRunning = false;
         }
     }
 
@@ -244,6 +282,7 @@ public class MainSceneController implements Initializable {
         refreshCodeSection();
         refreshRegisters();
         refreshMemory();
+        refreshLsQueue();
         setClockTime(currentTime);
     }
 
@@ -258,6 +297,18 @@ public class MainSceneController implements Initializable {
                     rRow.setBusy("Yes");
                     int op = thisBuffer.operator;
                     rRow.setOp(OP_NAME[i * 2 + op]);
+                    if (thisBuffer.rsIndex[0][0] != -1) {
+                        rRow.setQj(RS_NAME[thisBuffer.rsIndex[0][0]] +
+                                String.valueOf(thisBuffer.rsIndex[0][1] + 1));
+                    } else {
+                        rRow.setVj(String.valueOf(thisBuffer.value[0]));
+                    }
+                    if (thisBuffer.rsIndex[1][0] != -1) {
+                        rRow.setQk(RS_NAME[thisBuffer.rsIndex[1][0]] +
+                                String.valueOf(thisBuffer.rsIndex[1][1] + 1));
+                    } else {
+                        rRow.setVk(String.valueOf(thisBuffer.value[1]));
+                    }
                 } else {
                     rRow.setBusy("No");
                 }
@@ -282,14 +333,14 @@ public class MainSceneController implements Initializable {
     private void refreshRegisters() {
         ObservableList<CPURegRow> a = FXCollections.observableArrayList();
         for (int i = 0; i < 8; ++ i) {
-            CPURegRow cRow = new CPURegRow("R" + String.valueOf(i + 1));
+            CPURegRow cRow = new CPURegRow("R" + String.valueOf(i));
             cRow.setValue(String.valueOf(pipeline.cpuRegisters[i]));
             a.add(cRow);
         }
         cpuRegTableView.getItems().setAll(a);
         ObservableList<FPRegRow> b = FXCollections.observableArrayList();
-        for (int i = 0; i < 30; ++ i) {
-            FPRegRow fRow = new FPRegRow("F" + String.valueOf(i + 1));
+        for (int i = 0; i <= 30; i += 2) {
+            FPRegRow fRow = new FPRegRow("F" + String.valueOf(i));
             fRow.setValue(String.valueOf(pipeline.fpRegisters[i]));
             String rsName;
             if (pipeline.fpRegistersStatus[i][0] == -1) {
@@ -316,7 +367,38 @@ public class MainSceneController implements Initializable {
                 a.add(memRow);
             }
         }
-        memTableView.getItems().addAll(a);
+        memTableView.getItems().setAll(a);
+    }
+
+    private void refreshLsQueue() {
+        ObservableList<LsQueueRow> a = FXCollections.observableArrayList();
+        int id = 1;
+        for (Integer queueId : pipeline.loadStoreQueue) {
+            LsQueueRow lsQueueRow = new LsQueueRow();
+            lsQueueRow.setId(String.valueOf(id));
+            int rsId = queueId / 3;
+            int inId = queueId % 3;
+            TMLBuffer thisBuffer = pipeline.buffers[rsId + 2][inId];
+            if (!thisBuffer.busy) {
+                showAlert(Alert.AlertType.INFORMATION, "错误", "内部错误",
+                        "一个在LS队列中的队员对应的保留栈非busy");
+            }
+            lsQueueRow.setOp(OP_NAME[rsId + 4]);
+            lsQueueRow.setRsName(RS_NAME[rsId + 2] + String.valueOf(inId + 1));
+            if (rsId == 1) {
+                // Only store need to know qj and vj
+                if (thisBuffer.rsIndex[0][0] != -1) {
+                    lsQueueRow.setQj(RS_NAME[thisBuffer.rsIndex[0][0]] +
+                            String.valueOf(thisBuffer.rsIndex[0][1] + 1));
+                } else {
+                    lsQueueRow.setVj(String.valueOf(thisBuffer.value[0]));
+                }
+            }
+            lsQueueRow.setAddress(String.valueOf(thisBuffer.address));
+            a.add(lsQueueRow);
+            id += 1;
+        }
+        lsQueueTableView.getItems().setAll(a);
     }
 
     private void showAlert(Alert.AlertType type, String title, String header, String content) {
@@ -337,37 +419,120 @@ public class MainSceneController implements Initializable {
     }
 
     public void registerChangeClicked(ActionEvent actionEvent) {
-        Dialog<String> dialog = new Dialog<>();
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
         dialog.setTitle("更改寄存器");
-        dialog.setHeaderText("输入寄存器名以更改寄存器，以R开头的为CPU通用寄存器，以F开头的为浮点寄存器");
+        dialog.setHeaderText("输入寄存器名以更改寄存器，以R开头的为CPU通用寄存器，以F开头的为浮点寄存器\n" +
+                "浮点寄存器可以填写F0, F2, ..., F30，CPU寄存器可以填写R0, R1, ..., R7");
 
         // Set the button types.
         ButtonType loginButtonType = new ButtonType("确定", ButtonBar.ButtonData.OK_DONE);
         dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
 
-        TextArea tArea = new TextArea();
-        tArea.setPromptText("请在这里输入代码");
-        BorderPane borderPane = new BorderPane(tArea);
-        BorderPane.setMargin(borderPane, new Insets(20, 20, 20, 20));
+        GridPane gridPane = new GridPane();
+        TextField regTextField = new TextField();
+        regTextField.setPromptText("寄存器名");
+        Label equalLabel = new Label("=");
+        TextField regValueField = new TextField();
+        regValueField.setPromptText("寄存器值");
+        gridPane.add(regTextField, 0, 0);
+        gridPane.add(equalLabel, 1, 0);
+        gridPane.add(regValueField, 2, 0);
+        gridPane.setAlignment(Pos.CENTER);
+        GridPane.setMargin(equalLabel, new Insets(0, 20, 0, 20));
 
-        dialog.getDialogPane().setContent(borderPane);
+        dialog.getDialogPane().setContent(gridPane);
 
         // Convert the result to a username-password-pair when the login button is clicked.
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == loginButtonType) {
-                return tArea.getText();
+                return new Pair<>(regTextField.getText(), regValueField.getText());
             }
             return null;
         });
 
-        Optional<String> result = dialog.showAndWait();
+        Optional<Pair<String, String>> result = dialog.showAndWait();
 
         if (result.isPresent()) {
-            System.out.println("Good");
+            try {
+                Pair<String, String> res = result.get();
+                String reg = res.getKey().trim();
+                String val = res.getValue().trim();
+                if (reg.startsWith("R")) {
+                    int id = Integer.valueOf(reg.substring(1));
+                    int v = Integer.valueOf(val);
+                    if (id >= 0 && id <= 7) {
+                        pipeline.cpuRegisters[id] = v;
+                    } else {
+                        throw new ValueException("寄存器越界");
+                    }
+                } else if (reg.startsWith("F")) {
+                    int id = Integer.valueOf(reg.substring(1));
+                    float v = Float.valueOf(val);
+                    if (id >= 0 && id <= 30 && id % 2 == 0) {
+                        pipeline.fpRegisters[id] = v;
+                    } else {
+                        throw new ValueException("寄存器越界");
+                    }
+                } else {
+                    throw new ValueException("寄存器不识别");
+                }
+            } catch (Exception e) {
+                showAlert(Alert.AlertType.ERROR, "错误", "赋值错误", "赋值过程产生错误：\n" +
+                        e.getMessage() + "\n一个忠告：看清提示再填写。");
+            }
+            refreshRegisters();
         }
     }
 
     public void memoryChangeClicked(ActionEvent actionEvent) {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("更改内存");
+        dialog.setHeaderText("输入内存偏移（0-4095）和对应的值（浮点值）进行改变");
 
+        // Set the button types.
+        ButtonType loginButtonType = new ButtonType("确定", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        GridPane gridPane = new GridPane();
+        TextField regTextField = new TextField();
+        regTextField.setPromptText("内存偏移");
+        Label equalLabel = new Label("=");
+        TextField regValueField = new TextField();
+        regValueField.setPromptText("值");
+        gridPane.add(regTextField, 0, 0);
+        gridPane.add(equalLabel, 1, 0);
+        gridPane.add(regValueField, 2, 0);
+        gridPane.setAlignment(Pos.CENTER);
+        GridPane.setMargin(equalLabel, new Insets(0, 20, 0, 20));
+
+        dialog.getDialogPane().setContent(gridPane);
+
+        // Convert the result to a username-password-pair when the login button is clicked.
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(regTextField.getText(), regValueField.getText());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        if (result.isPresent()) {
+            try {
+                Pair<String, String> res = result.get();
+                String reg = res.getKey().trim();
+                String val = res.getValue().trim();
+                int offset = Integer.valueOf(reg);
+                if (offset < 0 || offset > 4095) {
+                    throw new ValueException("内存越界");
+                }
+                float v = Float.valueOf(val);
+                pipeline.memory[offset] = v;
+            } catch (Exception e) {
+                showAlert(Alert.AlertType.ERROR, "错误", "赋值错误", "赋值过程产生错误：\n" +
+                        e.getMessage() + "\n一个忠告：看清提示再填写。");
+            }
+            refreshMemory();
+        }
     }
 }
